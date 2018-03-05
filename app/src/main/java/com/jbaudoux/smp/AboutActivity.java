4c5e1ea207965894b158251c9293bdf8af5f4e4e
Jérôme Baudoux <contact@jerome-baudoux.com>
2018-03-05 20:54:14 +0100

    Add About page

--- app/src/main/java/com/jbaudoux/smp/AboutActivity.java
+++ app/src/main/java/com/jbaudoux/smp/AboutActivity.java
@@ -1,8 +1,12 @@
 package com.jbaudoux.smp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -18,6 +22,29 @@ public class AboutActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        Button gitHubButton = findViewById(R.id.aboutGitHubButton);
+        Button websiteButton = findViewById(R.id.abouWebsiteButton);
+
+        gitHubButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToWebsite("https://github.com/jerome-baudoux");
+            }
+        });
+        websiteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToWebsite("http://www.jerome-baudoux.com");
+            }
+        });
+    }
+
+    private void goToWebsite(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 
     @Override
